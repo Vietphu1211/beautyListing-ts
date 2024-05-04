05e83a256539d1611078d8b279a1f949a2b8d81d
vietphu1211 <vietphu1211@gmail.com>
2024-05-04 10:22:38 +0700

    cm1

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import Category from "./_components/category/Category";
 import Listings from "./_components/listings/Listings";
 import SearchPlace from "./_components/search/SearchPlace";
 import Blog from "./_components/Blog/Blog";
-import Feelback from "./_components/feelback/feelback";
 import Loading from "./Loading";
 import { useEffect, useState } from "react";
 import PostCardSkeleton from "./_components/skeleton/PostCardSkeleton";
@@ -11,6 +10,7 @@ import Header from "./_components/header/Header";
 import Footer from "./_components/footer/Footer";
 import Modal from "./_components/modals/Modal";
 import CategoryModal from "./_components/modals/CategoryModal";
+import Feelback from "./_components/feelback/Feelback";
 
 
 
