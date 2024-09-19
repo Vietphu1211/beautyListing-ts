c1ae511f4be83de09ef756c2c1e55bb9d3ab35bf
vietphu1211 <vietphu1211@gmail.com>
2024-09-19 17:15:38 +0700

    cm

--- app/(page)/listings/[slug]/NavigationTabs.jsx
+++ app/(page)/listings/[slug]/NavigationTabs.jsx
@@ -41,30 +41,30 @@ const NavigationTabs = () => {
 
 
     return (
-        
-            <div className=' flex flex-row justify-center items-center gap-2 overflow-x-auto
-                 '>
-                {
-                    tabData?.map((tab) => (
 
-                        <Link
-                            id={tab.anchorId}
-                            key={tab.anchorId}
-                            onClick={(e) => handleClick(e)}
-                            href={`/listing/apha#anchor-${tab.anchorId}`}
-                            //  href={`/listing/${listingId}#anchor-${tab.anchorId}
-                            className={`flex flex-col justify-center items-center min-w-[120px] h-12 w-15 px-2  duration-50
+        <div className=' flex flex-row justify-center items-center gap-2 overflow-x-auto
+                 '>
+            {
+                tabData?.map((tab) => (
+
+                    <Link
+                        id={tab.anchorId}
+                        key={tab.anchorId}
+                        onClick={(e) => handleClick(e)}
+                        href={`/listing/apha#anchor-${tab.anchorId}`}
+                        //  href={`/listing/${listingId}#anchor-${tab.anchorId}
+                        className={`flex flex-col justify-center items-center min-w-[120px] h-12 w-15 px-2  duration-50
             ${selectedTab === tab.anchorId ? "text-white bg-primary rounded-sm border-primary " : "text-neutral-200 bg-neutral-800  "}
             ${selectedTab === tab.anchorId ? " shadow-lg" : ""}
             `}>
-                            <p>
-                                {tab.title}
-                            </p>
-                        </Link>
-                    ))
-                }
-            </div>
-        
+                        <p>
+                            {tab.title}
+                        </p>
+                    </Link>
+                ))
+            }
+        </div>
+
     )
 }
 
